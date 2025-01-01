0195b9f9abfce651f96919a4f5ea45c943b163ff
wzqrxzd <wzqrxzd@arch>
2025-01-02 00:32:38 +0100

    init

--- waybar.py
+++
@@ -1,9 +0,0 @@
-import re
-
-class WaybarController:
-    def __init__(self, waybar_path):
-        pass
-
-if __name__ == "__main__":
-    WaybarController("/home/wzqrxzd/projects/theme_setup/oop/waybar.css")
-
